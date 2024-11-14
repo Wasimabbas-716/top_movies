e144d871cd18ec5c803ba9b156a5bcdc72467a72
Wasimabbas-716 <wasimabbas0051@gmail.com>
2024-11-14 17:59:13 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ import Header from './componet/Header';
 import Footer from './componet/Footer';
 const Home = () => {
   return (<>
+    <head>
+<meta name="google-adsense-account" content="ca-pub-1430143994998751"/>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1430143994998751"
+     crossorigin="anonymous"></script>
+</head>
   <Header/>
     <Nav/>
     <Poster/>
