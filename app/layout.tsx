cfd570d5d53587ca54717895cbea97ee1b916291
Wasimabbas-716 <wasimabbas0051@gmail.com>
2024-11-14 17:55:36 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head> 
+        <meta name="google-adsense-account" content="ca-pub-1430143994998751"/>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1430143994998751"
+     crossOrigin="anonymous"></script>
+      
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
